f9664d0f56f4f1aa96dd5275f45e2d9eacc9e430
Taterao Jinkalwar <122861107+Taterao3133@users.noreply.github.com>
2024-04-08 18:52:36 +0530

    added

--- client_side/src/App.js
+++ client_side/src/App.js
@@ -1,10 +1,8 @@
-
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-     fdfd
-    </div>
+    <Header/>
   );
 }
 
---
+++ client_side/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+
+
+function Header() {
+  return (
+    <div className="Header text-red-600">
+        <div className="flex flex-nowrap">
+            <div className="leftheader">
+                <div className="logo">ff</div>
+            </div>
+            <div className="rightheader">
+                <div className="contact">ff</div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
