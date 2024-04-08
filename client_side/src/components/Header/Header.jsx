26a6bff5cb64d0c47a6ee512a1aeabf05dd26730
Taterao Jinkalwar <122861107+Taterao3133@users.noreply.github.com>
2024-04-08 19:04:46 +0530

    headeer...

--- client_side/src/components/Header/Header.jsx
+++ client_side/src/components/Header/Header.jsx
@@ -2,13 +2,21 @@
 
 function Header() {
   return (
-    <div className="Header text-red-600">
+    <div className="Header">
         <div className="flex flex-nowrap">
             <div className="leftheader">
-                <div className="logo">ff</div>
+                <div className="logo">gh</div>
+                <div className="name">
+                    <p>Unique Beauty <span>Salon</span></p>
+                </div>
             </div>
             <div className="rightheader">
-                <div className="contact">ff</div>
+                <div className="headerlinks">
+                   <p>Home</p>
+                   <p>AboutUs</p>
+                   <p>Contact Us</p>
+                   
+                </div>
             </div>
         </div>
     </div>
