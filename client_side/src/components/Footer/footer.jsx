5fe444b4f23332613424d4eff15d87733e4afef1
Taterao Jinkalwar <122861107+Taterao3133@users.noreply.github.com>
2024-07-05 20:07:08 +0530

    fixes aome issues

---
+++ client_side/src/components/Footer/footer.jsx
@@ -0,0 +1,13 @@
+
+
+function Footer() {
+  return (
+   <>
+    <div className="footer ">
+      <div className="box w-full h-96 border-2 "> jfkglkfgk</div>
+    </div>
+    </>
+  )
+}
+
+export default Footer
--- client_side/src/components/Home/Home.jsx
+++ client_side/src/components/Home/Home.jsx
@@ -5,6 +5,7 @@ import { BsCalendar2Date,  } from "react-icons/bs";
 import Makeupicon from '../../Images/makeup1.svg'
 import { FaRegStar } from "react-icons/fa";
 import { SlLocationPin } from "react-icons/sl";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 
 
 import "@fontsource/manrope";
@@ -12,6 +13,7 @@ import "@fontsource/manrope";
 import exp1 from '../../Images/exp-pic3.jpg'
 import exp2 from '../../Images/exp-pic2.jpg'
 import exp3 from '../../Images/exp-pic4.jpg'
+
 
 function Home() {
   return <>
@@ -223,15 +225,42 @@ function Home() {
           </div>
 
         </div>
-
+      </div>
         {/* User reviews testmonial=============================== */}
-        <div className="container min-w-full h-[760px] text-center border-2 border-red-500 mt-16">
-          <p className='font-semibold text-[#BA7894]'> TESTIMONIALS.</p>
-          <p className='font-bold text-5xl font-playfair text-[#141414]'>
-            What our Constumers says....
-          </p>
-        </div>
-     </div>
+       <div class="container min-w-full h-[760px] text-center bg-[#EBF3F5]  -mt-9">
+            <p class='font-semibold text-[#BA7894] pt-6'>TESTIMONIALS</p>
+            <p class='font-bold text-5xl mt-1 font-playfair text-[#141414]'>
+              What our Customers Say
+            </p>
+            <div class="comment_box bg-[#422A3C] h-[500px] max-w-[1100px] mx-auto mt-14 rounded-3xl grid grid-cols-2 gap-4">
+              <div class="l_content mt-28 ml-56 flex flex-nowrap">
+                <div class="white_box w-10 h-32 bg-white rounded-3xl"></div>
+                <div class="white_box ml-3 w-10 h-60 bg-white rounded-3xl"></div>
+                <div class="img_user w-36 h-36  ml-16 mt-14 border-2 border-white rounded-full absolute">
+                  <img src={exp3} alt="" className='user_img object-cover h-36 w-36 p-1 rounded-full overflow-hidden'/>
+                  </div>
+              </div>
+              <div class="R_content text-left text-white ">
+                <div className="box-c h-80 max-w-lg mt-28 mr-8">
+                  <div className="c_section  ml-5">
+                    <div className="u_name font-semibold text-[20px]">Taterao</div>
+
+                  </div>
+                  <div className="c_section2 mt-10 ml-5 text-lg pr-10">
+                    this salon is wonderfull and amazing services here . You should try once here.. gfjfhgf dfjhs sdhghf sdghsfgjh sdgH
+                  </div>
+                </div>
+              </div>
+      
+            </div>
+            <div className="pageChange_buttons justify-center pt-10 flex flex-nowrap">
+              <div className="Bbtn  border-2 h-14 w-14 bg-[white]  rounded-full pl-4 pt-4 "><FaChevronLeft className=' t'/> </div>
+              <div className="Bbtn  border-2 h-14 w-14 bg-[white]  rounded-full pl-4 pt-4 "><FaChevronRight className=' t'/> </div>
+            </div>
+          </div>
+
+  
+    
   
   </>
     
