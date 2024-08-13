e4140a427a9880034ca1673763ef51a384ac4372
Taterao Jinkalwar <122861107+Taterao3133@users.noreply.github.com>
2024-08-13 16:00:25 +0530

    f

--- client_side/src/components/Layout/Layout.jsx
+++ client_side/src/components/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../Header/Header';
 import RouterConfg from '../Route/RouterConfg';
-import Footer from '../Footer/Footer';
+import Footer from '../Foot/Footer';
 
 
 function Layout() {
