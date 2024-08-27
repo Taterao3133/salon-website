bb885468ede6e58326553abbc9e215b27f10446f
Taterao Jinkalwar <122861107+Taterao3133@users.noreply.github.com>
2024-08-27 20:49:43 +0530

    dd

--- client_side/src/components/services/heroslider.jsx
+++ client_side/src/components/services/heroslider.jsx
@@ -50,6 +50,7 @@ function HeroSlider() {
     <div className='hero-section relative border-b-2 overflow-hidden'>
       <div className="bg-[#fff] md:h-auto xl:h-[700px] w-full xl:relative overflow-hidden">
         
+            
         {/* Small Device Image */}
         <div className="bx2 sm:hidden relative pt-16 opacity-40">
           <img
@@ -83,7 +84,7 @@ function HeroSlider() {
               <p className='font-lato hidden lg:block lg:-mt-0 text-[#939191] xl:text-xl lg:p-3 p-8 text-left '>{slides[currentSlide].text}</p>
             </div>
             <a href="https://wa.me/918331950396?text=Hello%20there!%20I%20would%20like%20to%20make%20an%20appointment." target="_blank" rel="noopener noreferrer"
-            className="relative z-10">
+            className="relative  lg:z-10 md:z-10 xl:z-10">
               <button className='bg-[#b0ebdf] md:ml-16 ml-10 mt-6  lg:mt-0 lg:ml-32 hover:scale-110 xl:mt-10 xl:ml-44  p-3 xl:p-4 rounded-full text-lg xl:text-xl tracking-wider font-montserrat font-medium'>
                 Book Now
               </button>
