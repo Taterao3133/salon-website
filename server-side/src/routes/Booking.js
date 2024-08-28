e05f4128db2079ade535eef1f335ae21d2ba7447
Taterao Jinkalwar <122861107+Taterao3133@users.noreply.github.com>
2024-08-28 17:07:15 +0530

    unwated code deleted

--- server-side/src/routes/Booking.js
+++
@@ -1,8 +0,0 @@
-// // src/routes/booking.js
-// const express = require('express');
-// const router = express.Router();
-// const { submitBooking } = require('../controllers/bookingController');
-
-// router.post('/booking', submitBooking);
-
-// module.exports = router;
