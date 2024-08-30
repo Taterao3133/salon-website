3a3347bc61bee0d0c0674af9fa1b4bca8c8b2981
Taterao Jinkalwar <122861107+Taterao3133@users.noreply.github.com>
2024-08-30 18:31:32 +0530

    bugs fixed and home ui changed

--- client_side/src/Pages/Courses.jsx
+++ client_side/src/Pages/Courses.jsx
@@ -1,4 +1,5 @@
 import courseimg1 from '../Images/course-heroimg.jpg'
+import courseimg2 from '../Images/course-heroimg2.jpg'
 import haircut1 from '../Images/haircut1.jpg'
 import makaup1 from '../Images/makaup1.jpg'
 import pedicure1 from '../Images/pedicure1.jpg'
@@ -7,41 +8,43 @@ function Courses() {
   return (
     <div className="ntg  overflow-hidden">
       <div className="h-auto w-full relative">
-        <img src={courseimg1} alt="" className='opacity-100' />
-        <div className="absolute inset-0 flex flex-col lg:mt-40  lg:ml-28 max-xl:ml-40 max-xl:mt-44    p-4">
-        <h2 className="text-3xl md:text-4xl lg:text-5xl font-playfair text-[#1f2c32]  font-bold mb-4">
-            Start Your Career <br /> In the Beauty Industry
-          </h2>
-          <p className="text-base text-[#576c57]  font-raleway  md:text-lg mb-6">
-            The Beauty Academy specializes in providing students with quality education 
-            and <br /> individualized training to maintain the high standards required by the Beauty Industry.
-          </p>
-          
-          <button className="bg-white hover:bg-green-600 active:bg-white active:text-green-400 w-32 xl:text-xl lg:px-1 lg:font-semibold lg:rounded-2xl lg:py-3 xl:w-48 text-black py-2 px-4 rounded-md">
-            Register Now
-          </button>
+        <img src={courseimg1} alt="" className='opacity-100 max-sm:hidden'  />
+        <img src={courseimg2} alt="" className='opacity-100 md:hidden lg:hidden xl:hidden'  />
+        <div className="absolute inset-0 flex flex-col max-sm:mt-10 max-sm:ml-7 md:mt-10 md:ml-14 lg:mt-40  lg:ml-28 max-xl:ml-40 max-xl:mt-44    p-4">
+          <h2 className="text-3xl md:text-4xl lg:text-5xl font-playfair text-[#1f2c32]  font-bold mb-4">
+              Start Your Career <br /> <span className='text-2xl md:text-3xl lg:text-4xl'> In the Beauty Industry</span>
+            </h2>
+            <p className="text-base text-[#576c57]  font-raleway md:pr-72  md:text-lg mb-6">
+              The Beauty Academy specializes in providing students with quality education 
+             <span className='md:hidden'> and <br /> individualized training to maintain the high standards required by the Beauty Industry.</span>
+            </p>
+            <a href="https://wa.me/+918688664812?text=Hello%20there!%20I%20I%20want%20to%20to%20join%20a%20course%20Send%20me%20details." target="_blank" rel="noopener noreferrer"
+            className="">
+            <button className="bg-white hover:bg-green-600 hover:text-white active:bg-white active:text-gray-500 w-32 xl:text-xl lg:px-1 lg:font-semibold lg:rounded-2xl lg:py-3 xl:w-48 text-black py-2 px-4 rounded-md">
+              Register Now
+            </button></a>
         </div>
       </div>
       {/* choose section */}
-      <div className="cors-d items-center justify-center text-center bg-[#f6f9f9] xl:pt-24 h-auto">
-        <h1 className='text-3xl md:text-4xl lg:text-4xl xl:text-5xl font-playfair font-medium m-auto'>Why Choose Us</h1>
-        <p className='font-raleway text-base lg:mt-4 text-gray-800'> Join our academy to gain hands-on training and expertise in the beauty industry. Our personalized courses, <br /> led by experienced professionals, will equip you with the skills to excel. Start your journey with us and build a successful career in beauty.</p>
+      <div className="cors-d items-center justify-center text-center bg-[#f6f9f9] max-sm:pt-7 md:pt-7 xl:pt-24 h-auto">
+        <h1 className='text-3xl md:text-4xl md:py-3 lg:text-4xl xl:text-5xl font-playfair font-medium m-auto'>Why Choose Us</h1>
+        <p className='font-raleway text-base max-sm:px-7 max-sm:py-3 max-sm:text-sm  md:px-20 lg:mt-4 text-gray-800'> Our courses offer comprehensive training in the latest beauty techniques, taught by industry professionals. Whether you're a beginner or looking to enhance your skills, our curriculum is designed to help you succeed in the beauty industry.</p>
         <div className="container w-[75%] pb-10  h-auto md:mt-5 m-auto xl:mt-7 grid grid-cols-2 max-sm:grid-cols-1 gap-6">
-          <div className="bx1 h-56 w-[100%] bg-white text-left pl-9 pt-9 hover:drop-shadow-xl shadow-lg rounded-lg">
-            <h3 className='font-montserrat font-bold text-2xl'>Classroom Courses</h3>
-            <p className='font-raleway text-base mt-3 text-gray-700 pr-5'>Experience in-person learning with our classroom courses, where expert instructors guide you through practical, hands-on training. Gain the skills you need to excel in the beauty industry with personalized attention and real-world practice.</p>
+          <div className="bx1 h-56 md:h-auto w-[100%] bg-white text-left max-sm:px-4 max-sm:py-4 md:py-4 md:pt-4 pl-9 pt-9 hover:drop-shadow-xl shadow-lg rounded-lg">
+            <h3 className='font-montserrat font-bold max-sm:text-lg md:text-xl max-sm:text-center text-2xl'>Classroom Courses</h3>
+            <p className='font-raleway max-sm:text-sm md:text-base max-sm:px-2 max-sm:text-center text-base mt-3 text-gray-700 pr-5'>Experience in-person learning with our classroom courses, where expert instructors guide you through practical, hands-on training. Gain the skills you need to excel in the beauty industry with personalized attention and real-world practice.</p>
           </div>
-          <div className="bx1 h-56 w-[100%] bg-white text-left pl-9 pt-9 hover:drop-shadow-xl  shadow-lg rounded-lg">
-            <h3 className='font-montserrat font-bold text-2xl'>Best Beauty Expert</h3>
-            <p className='font-raleway text-base mt-3 text-gray-700 pr-5'>Learn from the best in the industry with our top beauty experts who bring years of experience and cutting-edge techniques. Achieve excellence with personalized guidance from professionals dedicated to your success.</p>
+          <div className="bx1 h-56 md:h-auto w-[100%] bg-white text-left max-sm:px-4 max-sm:py-4 md:py-4 pl-9 pt-9 hover:drop-shadow-xl  shadow-lg rounded-lg">
+            <h3 className='font-montserrat font-bold max-sm:text-lg md:text-xl max-sm:text-center text-2xl'>Best Beauty Expert</h3>
+            <p className='font-raleway max-sm:text-sm max-sm:px-2 max-sm:text-center md:text-base text-base mt-3 text-gray-700 pr-5'>Learn from the best in the industry with our top beauty experts who bring years of experience and cutting-edge techniques. Achieve excellence with personalized guidance from professionals dedicated to your success.</p>
           </div>
-          <div className="bx1 h-56 w-[100%] bg-white text-left pl-9 pt-9 hover:drop-shadow-xl shadow-lg rounded-lg">
-            <h3 className='font-montserrat font-bold text-2xl'>Quality & Standard </h3>
-            <p className='font-raleway text-base mt-3 text-gray-700 pr-5'>Our courses are designed to uphold the highest standards of excellence, ensuring you receive a top-tier education in beauty. With a focus on quality, we prepare you to meet and exceed industry expectations.</p>
+          <div className="bx1 h-56 md:h-auto w-[100%] bg-white text-left max-sm:px-4 max-sm:py-4 md:py-4 pl-9 pt-9 hover:drop-shadow-xl shadow-lg rounded-lg">
+            <h3 className='font-montserrat font-bold max-sm:text-lg md:text-xl max-sm:text-center text-2xl'>Quality & Standard </h3>
+            <p className='font-raleway max-sm:text-sm md:text-base max-sm:px-2 max-sm:text-center text-base mt-3 text-gray-700 pr-5'>Our courses are designed to uphold the highest standards of excellence, ensuring you receive a top-tier education in beauty. With a focus on quality, we prepare you to meet and exceed industry expectations.</p>
           </div>
-          <div className="bx1 h-56 w-[100%] bg-white text-left pl-9 pt-9 hover:drop-shadow-xl shadow-lg rounded-lg">
-            <h3 className='font-montserrat font-bold text-2xl'>ISO Certified</h3>
-            <p className='font-raleway text-base mt-3 text-gray-700 pr-5'>As an ISO-certified institution, we are committed to maintaining the highest standards of quality and excellence. Our certification ensures that you receive a globally recognized education that meets rigorous industry benchmarks.</p>
+          <div className="bx1 h-56 md:h-auto w-[100%] bg-white text-left max-sm:px-4 max-sm:py-4 md:py-4 pl-9 pt-9 hover:drop-shadow-xl shadow-lg rounded-lg">
+            <h3 className='font-montserrat font-bold max-sm:text-lg md:text-xl max-sm:text-center text-2xl'>ISO Certified</h3>
+            <p className='font-raleway max-sm:text-sm md:text-base max-sm:px-2 max-sm:text-center text-base mt-3 text-gray-700 pr-5'>As an ISO-certified institution, we are committed to maintaining the highest standards of quality and excellence. Our certification ensures that you receive a globally recognized education that meets rigorous industry benchmarks.</p>
           </div>
         
           
@@ -50,41 +53,49 @@ function Courses() {
       </div>
 
       {/* course section */}
-      <div className="cors-d items-center justify-center text-center bg-[#fbfcfc] lg:pt-20 xl:pt-24 h-auto">
+      <div className="cors-d items-center justify-center text-center bg-[#fbfcfc] max-sm:pt-7 md:pt-7 lg:pt-20 xl:pt-24 h-auto">
         <h1 className='text-3xl md:text-4xl lg:text-4xl xl:text-5xl font-playfair font-medium m-auto'>Our Courses</h1>
-        <p className='font-raleway text-base lg:px-24 lg:mt-4 text-gray-800'> Join our academy to gain hands-on training and expertise in the beauty industry. Our personalized courses, <br /> led by experienced professionals, will equip you with the skills to excel. Start your journey with us and build a successful career in beauty.</p>
-        <div className="container w-[75%] lg:w-[80%] pb-10  h-auto md:mt-5 m-auto xl:mt-7 max-md:grid-cols-1 grid lg:grid-cols-3 max-sm:grid-cols-1 lg:gap-2 md:gap-4 xl:gap-6">
-          <div className="bx1 h-[435px] md:h-auto md:pb-5 w-[100%] bg-white text-left p-2  hover:drop-shadow-xl shadow-lg rounded-xl">
+        <p className='font-raleway text-base max-sm:px-7 max-sm:py-3 md:py-3 md:px-20 lg:px-24 lg:mt-4 text-gray-800'> Join our academy to gain hands-on training and expertise in the beauty industry. Our personalized courses, led by experienced professionals, will equip you with the skills to excel. Start your journey with us and build a successful career in beauty.</p>
+        <div className="container w-[75%] lg:w-[80%] max-sm:gap-y-3 pb-10  h-auto md:mt-5 m-auto xl:mt-7 max-md:grid-cols-1 grid lg:grid-cols-3 max-sm:grid-cols-1 lg:gap-2 md:gap-4 xl:gap-6">
+          <div className="bx1 h-[435px] md:h-auto md:pb-5 max-sm:h-auto max-sm:pb-3 w-[100%] bg-white text-left p-2  hover:drop-shadow-xl shadow-lg rounded-xl">
             <img src={pedicure1} alt="" className='lg:h-56 lg:w-96 md:w-full md:h-72 object-cover rounded-xl ' />
             <h3 className='text-2xl font-bold font-playfair tracking-tight mt-5 ml-5 text-[#442e46]'>Pedicure</h3>
-            <p className='text-gray-600 text-base font-lato tracking-wide mt-3 ml-5'>Pampering your feet with precision and care for a flawless finish.</p>
-            <div className="flex xl:mt-7 2xl:mt-7 lg:mt-3 justify-between lg:px-2 xl:px-5 md:mt-7 px-5">
-            <button className='bg-blue-600 text-lg max-lg:text-[16px]   font-playfair tracking-wide font-semibold text-white rounded-xl 2xl:px-3 xl:px-3 lg:px-2 p-2  px-3 hover:text-black hover:bg-white active:drop-shadow-lg hover:drop-shadow-2xl active:text-purple-400'>Register Now</button>
-            <h4 className='text-lg font-playfair font-bold rounded-xl text-white bg-pink-400 p-2 px-3'>Don't miss</h4>
-
+            <p className='text-gray-600 text-base font-lato tracking-wide max-sm:mt-2 max-sm:text-sm mt-3 ml-5'>Pampering your feet with precision and care for a flawless finish.</p>
+            <div className="flex xl:mt-7 max-sm:mt-3 2xl:mt-7 lg:mt-3 justify-between lg:px-2 xl:px-5 md:mt-7 px-5">
+              <a href="https://wa.me/+918688664812?text=Hello%20there!%20I%20I%20want%20to%20to%20join%20a%20course%20Send%20me%20details." target="_blank" rel="noopener noreferrer"
+              className="">
+              <button className='bg-blue-600 text-lg max-lg:text-[16px] max-sm:text-base  font-playfair tracking-wide font-semibold text-white rounded-xl 2xl:px-3 xl:px-3 lg:px-2 p-2  px-3 hover:text-black hover:bg-white active:drop-shadow-lg hover:drop-shadow-2xl active:text-purple-400'>Register Now</button>
+              </a>
+              <h4 className='text-lg font-playfair font-bold rounded-xl text-white bg-pink-400 max-sm:px-2 max-sm:text-base p-2 px-3'>Don't miss</h4>
             </div>
              
           </div>
 
-          <div className="bx1 h-[435px] md:h-auto md:pb-5 w-[100%] bg-white text-left p-2  hover:drop-shadow-xl shadow-lg rounded-xl">
+          <div className="bx1 h-[435px]  max-sm:h-auto max-sm:pb-3 md:h-auto md:pb-5 w-[100%] bg-white text-left p-2  hover:drop-shadow-xl shadow-lg rounded-xl">
             <img src={haircut1} alt="" className='lg:h-56 lg:w-96 md:w-full md:h-72 object-cover rounded-xl ' />
             <h3 className='text-2xl font-bold font-playfair tracking-tight mt-5 ml-5 text-[#442e46]'>Haircut</h3>
-            <p className='text-gray-600 text-base font-lato tracking-wide mt-3 ml-5'>Transforming your look with expert cuts tailored to your style.</p>
-            <div className="flex md:mt-7 xl:mt-7 2xl:mt-7 lg:mt-3 justify-between lg:px-2 xl:px-5 px-5">
-            <button className='bg-blue-600 text-lg   font-playfair tracking-wide font-semibold text-white rounded-xl p-2 px-3 2xl:px-3 xl:px-3 lg:px-2 hover:text-black hover:bg-white active:drop-shadow-lg hover:drop-shadow-2xl active:text-purple-400'>Register Now</button>
-            <h4 className='text-lg font-playfair font-bold rounded-xl text-white bg-pink-400 p-2 px-3'>Don't miss </h4>
+            <p className='text-gray-600 text-base max-sm:mt-2 max-sm:text-sm font-lato tracking-wide mt-3 ml-5'>Transforming your look with expert cuts tailored to your style.</p>
+            <div className="flex md:mt-7 max-sm:mt-3 xl:mt-7 2xl:mt-7 lg:mt-3 justify-between lg:px-2 xl:px-5 px-5">
+              <a href="https://wa.me/+918688664812?text=Hello%20there!%20I%20I%20want%20to%20to%20join%20a%20course%20Send%20me%20details." target="_blank" rel="noopener noreferrer"
+              className="">
+              <button className='bg-blue-600 text-lg max-sm:text-base  font-playfair tracking-wide font-semibold text-white rounded-xl p-2 px-3 2xl:px-3 xl:px-3 lg:px-2 hover:text-black hover:bg-white active:drop-shadow-lg hover:drop-shadow-2xl active:text-purple-400'>Register Now</button>
+              </a>
+              <h4 className='text-lg font-playfair font-bold rounded-xl text-white bg-pink-400 max-sm:px-2 max-sm:text-base p-2 px-3'>Don't miss </h4>
 
             </div>
              
           </div>
 
-          <div className="bx1 h-[435px] md:h-auto md:pb-5 w-[100%] bg-white text-left p-2  hover:drop-shadow-xl shadow-lg rounded-xl">
+          <div className="bx1 h-[435px] md:h-auto max-sm:h-auto max-sm:pb-3 md:pb-5 w-[100%] bg-white text-left p-2  hover:drop-shadow-xl shadow-lg rounded-xl">
             <img src={makaup1} alt="" className='lg:h-56 lg:w-96 md:w-full md:h-72 object-cover rounded-xl ' />
             <h3 className='text-2xl font-bold font-playfair tracking-tight mt-5 ml-5 text-[#442e46]'>Makaup</h3>
-            <p className='text-gray-600 text-base font-lato tracking-wide mt-3 ml-5'>Empowering students with engaging, interactive learning experiences.</p>
-            <div className="flex md:mt-7 xl:mt-7 2xl:mt-7 lg:mt-3 justify-between lg:px-2 xl:px-5 px-5">
-            <button className='bg-blue-600 text-lg   font-playfair tracking-wide font-semibold text-white rounded-xl xl:p-2 2xl:p-2  px-3 2xl:px-3 xl:px-3 lg:px-2  hover:text-black hover:bg-white active:drop-shadow-lg hover:drop-shadow-2xl active:text-purple-400'>Register Now</button>
-            <h4 className='text-lg font-playfair font-bold rounded-xl text-white bg-pink-400 p-2 px-3'>Don't miss</h4>
+            <p className='text-gray-600 text-base font-lato tracking-wide max-sm:mt-2 max-sm:text-sm mt-3 ml-5'>Empowering students with engaging, interactive learning experiences.</p>
+            <div className="flex md:mt-7 max-sm:mt-3 xl:mt-7 2xl:mt-7 lg:mt-3 justify-between lg:px-2 xl:px-5 px-5">
+              <a href="https://wa.me/+918688664812?text=Hello%20there!%20I%20I%20want%20to%20to%20join%20a%20course%20Send%20me%20details." target="_blank" rel="noopener noreferrer"
+              className="">
+              <button className='bg-blue-600 text-lg max-sm:text-base  font-playfair tracking-wide font-semibold text-white rounded-xl xl:p-2 2xl:p-2  px-3 2xl:px-3 xl:px-3 lg:px-2  hover:text-black hover:bg-white active:drop-shadow-lg hover:drop-shadow-2xl active:text-purple-400'>Register Now</button>
+              </a>
+              <h4 className='text-lg font-playfair font-bold rounded-xl text-white bg-pink-400 max-sm:px-2 max-sm:text-base p-2 px-3'>Don't miss</h4>
 
             </div>
              
--- client_side/src/components/services/heroslider.jsx
+++ client_side/src/components/services/heroslider.jsx
@@ -5,7 +5,7 @@ import Homeslide3 from '../../Images/home-slide-3.png';
 import Homeslide4 from '../../Images/home-slide-3sm.png';
 import Homeslide5 from '../../Images/home-slide-2sm.png';
 import Homeslide6 from '../../Images/home-slide-1sm.png';
-
+import heroimg2 from '../../Images/course-heroimg2.jpg'
 
 const slides = [
   {
@@ -48,20 +48,21 @@ function HeroSlider() {
 
   return (
     <div className='hero-section relative border-b-2  overflow-hidden'>
-      <div className="bg-[#fff] md:h-auto xl:h-[650px]  max-lg:h-[400px] w-full xl:relative overflow-hidden">
+      <div className="bg-[#fff] md:h-auto xl:h-[650px] max-sm:h-auto max-lg:h-[400px] w-full xl:relative overflow-hidden">
         
-            
+            <img src={heroimg2} alt="" className='md:hidden lg:hidden xl:hidden' /> 
         {/* Small Device Image */}
-        <div className="bx2 sm:hidden relative  pt-10 opacity-40">
+        {/* <div className="bx2 max-sm:hidden relative  pt-10 opacity-40">
           <img
             src={slides[currentSlide].smallImage}
             alt=""
             className={`h-[400px] w-full transition-transform duration-500`}
           />
-        </div>
+        </div> */}
+
 
         {/* Large Device Background Circles */}
-        <div className="bg-rounds hidden sm:block">
+        <div className="bg-rounds max-sm:hidden  sm:block">
           <div className="xl:ml-[750px] md:ml-[360px] md:-mt-20 xl:-mt-44 bg-[#cef1ea] md:w-32 md:h-32 xl:h-60 xl:w-60 rounded-full"></div>
           <div className="bg-[#cef1ea]  md:ml-[450px] md:opacity-60 lg:ml-[630px]  lg:h-[400px] lg:w-[400px] xl:mt-20 2xl:mt-16 2xl:ml-[890px] md:h-[300px] md:w-[300px] 2xl:h-[600px] 2xl:w-[600px] rounded-full"></div>
         </div>
@@ -77,20 +78,26 @@ function HeroSlider() {
 
         <div className="hero-flex flex absolute top-0 left-0 w-full h-full  md:overflow-hidden ">
           <div className="bx1 xl:flex-[0.6] md:flex[0.6] lg:flex-[0.6]  h-full xl:mt-5 2xl:mt-10">
-            <div className={`heading text-left max-sm:ml-10 md:ml-16 lg:ml-32 2xl:ml-44 md:mt-28 lg:mt-28 max-sm:mt-48 xl:mt-40 2xl:mt-48 transition-transform duration-1000 ${isAnimating ? 'translate-y-[-50px] opacity-0' : 'translate-y-0 opacity-100'}`}>
-              <h2 className='text-2xl md:text-3xl xl:text-5xl font-playfair tracking-normal font-bold text-[#151515]'>{slides[currentSlide].heading}</h2>
+            <div className={`heading text-left max-sm:ml-10 md:ml-16 lg:ml-32 2xl:ml-44 md:mt-28 lg:mt-28 max-sm:mt-20 xl:mt-40 2xl:mt-48 transition-transform duration-1000 ${isAnimating ? 'translate-y-[-50px] opacity-0' : 'translate-y-0 opacity-100'}`}>
+              <h2 className='text-3xl md:text-3xl xl:text-5xl font-playfair tracking-normal font-bold text-[#151515]'>{slides[currentSlide].heading}</h2>
               <hr className='border-[#b0ebdf] hidden md:block mt-5 xl:mt-10  md:ml-1 md:w-10 xl:w-20 border-[2px]' />
               <h2 className='xl:-mt-8 md:-mt-5 xl:ml-24  md:ml-12 text-2xl md:text-3xl xl:text-5xl font-playfair text-[#151515] font-bold'>{slides[currentSlide].subheading}</h2>
-              <p className='font-lato hidden lg:block lg:-mt-0 text-[#939191] xl:text-xl lg:p-3 p-8 text-left '>{slides[currentSlide].text}</p>
+              <p className='font-lato md:hidden max-sm:pl-0 max-sm:pt-2 lg:block lg:-mt-0 text-[#939191] xl:text-xl lg:p-3 p-8 text-left '>{slides[currentSlide].text}</p>
             </div>
             
             <a href="https://wa.me/+918688664812?text=Hello%20there!%20I%20would%20like%20to%20make%20an%20appointment." target="_blank" rel="noopener noreferrer"
-            className="relative  z-10">
-              <button className='bg-[#b0ebdf] md:ml-16 ml-10 mt-6  lg:mt-0 lg:ml-32 hover:scale-110 xl:mt-10 xl:ml-44  p-3 xl:p-4 rounded-full text-lg xl:text-xl tracking-wider font-montserrat font-medium'>
+            className="relative max-sm:hidden  z-10">
+              <button className='bg-[#b0ebdf] max-sm:hidden md:ml-16 ml-10 mt-6  lg:mt-0 lg:ml-32 hover:scale-110 xl:mt-10 xl:ml-44  p-3 xl:p-4 rounded-full text-lg xl:text-xl tracking-wider font-montserrat font-medium'>
                 Book Now 
               </button>
             </a>
-            
+            <div className="">
+            <a href="https://wa.me/+918688664812?text=Hello%20there!%20I%20would%20like%20to%20make%20an%20appointment." target="_blank" rel="noopener noreferrer"
+            className="">
+              <button className="bg-white md:hidden ml-10 mt-2 hover:bg-green-600 hover:text-white active:bg-white active:text-gray-500 w-32 xl:text-xl lg:px-1 lg:font-semibold lg:rounded-2xl lg:py-3 xl:w-48 text-black py-2 px-4 rounded-md">
+              Register Now
+            </button></a>
+            </div>
           </div>
 
           {/* Large Device Image */}
