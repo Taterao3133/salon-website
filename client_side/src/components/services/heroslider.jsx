6f2d85d03fc1eb62ae1e2e0cc4a8fd1c91830ef8
Taterao Jinkalwar <122861107+Taterao3133@users.noreply.github.com>
2024-08-31 12:37:51 +0530

    f

--- client_side/src/components/services/heroslider.jsx
+++ client_side/src/components/services/heroslider.jsx
@@ -12,6 +12,7 @@ const slides = [
     heading: 'Unique Beauty Salon',
     subheading: 'Beauty as a gift,..',
     text: 'Unique Beauty Salon offers the latest and highest quality services for you and - all your family members. We specialize in all beauty treatments and our team is fully professional and innovative, from makeup services to hairstyling.',
+    smText:'Unique Beauty Salon offers the latest and highest quality services for you and - all your family members.',
     image: Homeslide1,
     smallImage: Homeslide4
   },
@@ -19,6 +20,7 @@ const slides = [
     heading: 'Experience in Beauty',
     subheading: 'Your Beauty Salon ',
     text: 'At Unique Beauty Salon, we provide a personalized experience for each client. Our expert stylists and beauticians are dedicated to enhancing your natural beauty with the latest trends and techniques.',
+    smText:'At Unique Beauty Salon, we provide a personalized experience for each client. ',
     image: Homeslide2,
     smallImage: Homeslide5
   },
@@ -26,6 +28,7 @@ const slides = [
     heading: 'Professional - Beauty',
     subheading: "Care for Everyone",
     text: "Unique Beauty salon treatments will help you relax after a long and stressful day. At Unique Beauty, you can pamper yourself and enjoy the benefits of professional and affordable beauty care. Enjoy your life with our high-quality beauty services.",
+    smText:'Unique Beauty salon treatments will help you relax after a long and stressful day.',
     image: Homeslide3,
     smallImage: Homeslide6
   }
@@ -51,6 +54,7 @@ function HeroSlider() {
       <div className="bg-[#fff] md:h-auto xl:h-[650px] max-sm:h-auto max-lg:h-[400px] w-full xl:relative overflow-hidden">
         
             <img src={heroimg2} alt="" className='md:hidden lg:hidden xl:hidden' /> 
+
         {/* Large Device Background Circles */}
         <div className="bg-rounds max-sm:hidden  sm:block">
           <div className="xl:ml-[750px] md:ml-[360px] md:-mt-20 xl:-mt-44 bg-[#cef1ea] md:w-32 md:h-32 xl:h-60 xl:w-60 rounded-full"></div>
@@ -66,13 +70,14 @@ function HeroSlider() {
           />
         </div>
 
-        <div className="hero-flex flex absolute top-0 left-0 w-full h-full  md:overflow-hidden ">
+        <div className="hero-flex  flex absolute top-0 left-0 w-full h-full  md:overflow-hidden ">
           <div className="bx1 xl:flex-[0.6] md:flex[0.6] lg:flex-[0.6]  h-full xl:mt-5 2xl:mt-10">
-            <div className={`heading text-left max-sm:ml-10 md:ml-16 lg:ml-32 2xl:ml-44 md:mt-28 lg:mt-28 max-sm:mt-20 xl:mt-40 2xl:mt-48 transition-transform duration-1000 ${isAnimating ? 'translate-y-[-50px] opacity-0' : 'translate-y-0 opacity-100'}`}>
+            <div className={`heading text-left max-sm:ml-7 md:ml-16 max-sm:h-40  lg:ml-32 2xl:ml-44 md:mt-28 lg:mt-28 max-sm:mt-14 xl:mt-40 2xl:mt-48 transition-transform duration-1000 ${isAnimating ? 'translate-y-[-50px] opacity-0' : 'translate-y-0 opacity-100'}`}>
               <h2 className='text-3xl md:text-3xl xl:text-5xl font-playfair tracking-normal font-bold text-[#151515]'>{slides[currentSlide].heading}</h2>
               <hr className='border-[#b0ebdf] hidden md:block mt-5 xl:mt-10  md:ml-1 md:w-10 xl:w-20 border-[2px]' />
               <h2 className='xl:-mt-8 md:-mt-5 xl:ml-24  md:ml-12 text-2xl md:text-3xl xl:text-5xl font-playfair text-[#151515] font-bold'>{slides[currentSlide].subheading}</h2>
-              <p className='font-lato md:hidden max-sm:pl-0 max-sm:pt-2 lg:block lg:-mt-0 text-[#939191] xl:text-xl lg:p-3 p-8 text-left '>{slides[currentSlide].text}</p>
+              <p className='font-lato md:hidden max-sm:hidden lg:block lg:-mt-0 text-[#939191] xl:text-xl lg:p-3 p-8 text-left '>{slides[currentSlide].text}</p>
+              <p className='font-lato md:hidden lg:hidden x:hidden 2xl:hidden max-sm:pl-0 max-sm:pt-2 max-sm:pb-2  text-[#939191] p-8 text-left '>{slides[currentSlide].smText}</p>
             </div>
             
             <a href="https://wa.me/+918688664812?text=Hello%20there!%20I%20would%20like%20to%20make%20an%20appointment." target="_blank" rel="noopener noreferrer"
@@ -81,10 +86,10 @@ function HeroSlider() {
                 Book Now 
               </button>
             </a>
-            <div className="">
+            <div className="absolute">
             <a href="https://wa.me/+918688664812?text=Hello%20there!%20I%20would%20like%20to%20make%20an%20appointment." target="_blank" rel="noopener noreferrer"
             className="">
-              <button className="bg-white md:hidden ml-10 mt-2 hover:bg-green-600 hover:text-white active:bg-white active:text-gray-500 w-32 xl:text-xl lg:px-1 lg:font-semibold lg:rounded-2xl lg:py-3 xl:w-48 text-black py-2 px-4 rounded-md">
+              <button className="bg-white md:hidden ml-6 -mt-10 hover:bg-green-600 hover:text-white active:bg-white active:text-gray-500 w-32 xl:text-xl lg:px-1 lg:font-semibold lg:rounded-2xl lg:py-3 xl:w-48 text-black py-2 px-4 rounded-md">
               Register Now
             </button></a>
             </div>
